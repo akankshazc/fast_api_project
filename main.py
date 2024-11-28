cc218c6114ffbf081bbfb4303986825d186fdfa1
Akanksha <akankshazc23@icloud.com>
2024-11-28 16:59:23 +0530

    pydantic model for posts and validating post request body

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -14,6 +23,11 @@ def get_posts():
 
 
 @app.post("/post")
-def post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
+def post(new_post: Post):
+
+    # automatic validating the request body
+    print(new_post)
+
+    # to print the new_post pydantic model as a dictionary
+    # new_post.dict() method is deprecated
+    return {"data": new_post.model_dump()}
