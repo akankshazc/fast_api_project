90d3084ad54fe7ed94b6f27ec6feb80e0de0d725
Akanksha <akankshazc23@icloud.com>
2024-12-18 08:01:11 +0530

    add route to retrieve a particular post with id
    Following changes are made:
    
    - Added a new route to retrieve a particular post with id
    - HTTP exception to handle the case when the post with the given id is not found
    - Added a new function to retrieve a particular post with id
    - Added comments to all path operations
    - Changed the HTTP status code for the post creation route

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -13,23 +13,34 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+# Sample data
 my_posts = [{"title": "Post 1", "content": "This is the content of post 1", "id": 1,
              "published": True, "rating": 5},
             {"title": "Post 2", "content": "This is the content of post 2", "id": 2,
              "published": True, "rating": 4}]
 
 
+# Function to find a single post
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+
+# Root path
 @app.get("/")
 def root():
     return {"message": "Welcome to the API"}
 
 
+# Retrieving all posts
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/post")
+# Creating a new post
+@app.post("/post", status_code=status.HTTP_201_CREATED)
 def post(new_post: Post):
 
     # to convert the new_post pydantic model as a dictionary
@@ -40,3 +51,21 @@ def post(new_post: Post):
     my_posts.append(post_dict)
 
     return {"data": post_dict}
+
+
+# Retrieving a single post
+@app.get("/post/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+
+    # if post is not found return a 404 response
+    # if not post:
+    #     response.status_code = status.HTTP_404_NOT_FOUND
+    #     return {"message": f"Post with id {id} was not found"}
+
+    # using HTTPException
+    if not post:
+        raise HTTPException(status_code=404, detail=f"Post with id {
+                            id} was not found")
+
+    return {"data": post}
